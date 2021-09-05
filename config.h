b3913e05f1aa24820adcd0a217ca7b967497915d
ted1922 <59594230+ted1922@users.noreply.github.com>
2021-09-05 11:01:34 +1000

    update config.h with commands and set Mod|Shift Equals to default gaps of 4

--- config.h
+++ config.h
@@ -6,12 +6,17 @@ static const unsigned int gappx     = 4;        /* gaps between windows */
 static const unsigned int snap      = 32;       /* snap pixel */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
-static const char *fonts[]          = { "Fira Code:size=11:style=bold" };
-static const char dmenufont[]       = "Fira Code:size=11";
+static const char *fonts[]          = { "Fira Code:size=12:style=bold", "Fontawesome:size=12" };
+static const char dmenufont[]       = "Fira Code:size=12";
+// background colour
 static const char col_gray1[]       = "#222222";
+// inactive window border colour
 static const char col_gray2[]       = "#444444";
+// font colour
 static const char col_gray3[]       = "#bbbbbb";
+// current tag and current window font colour 
 static const char col_gray4[]       = "#eeeeee";
+// top bar second colour and active window border colour
 static const char col_cyan[]        = "#005577";
 static const char *colors[][3]      = {
 	/*               fg         bg         border   */
@@ -92,7 +97,7 @@ static Key keys[] = {
 	{ MODKEY|ShiftMask,             XK_period, tagmon,         {.i = +1 } },
 	{ MODKEY,                       XK_minus,  setgaps,        {.i = -1 } },
 	{ MODKEY,                       XK_equal,   setgaps,        {.i = +1 } },
- 	{ MODKEY|ShiftMask,             XK_equal,  setgaps,        {.i = 0  } },	
+ 	{ MODKEY|ShiftMask,             XK_equal,  setgaps,        {.i = 4  } },	
 	TAGKEYS(                        XK_1,                      0)
 	TAGKEYS(                        XK_2,                      1)
 	TAGKEYS(                        XK_3,                      2)
