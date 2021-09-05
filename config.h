cba05e92a51415c7a5a876418bd5f3d9f78762a5
ted1922 <59594230+ted1922@users.noreply.github.com>
2021-09-05 11:13:06 +1000

    added gruvbox colours & updated readme

--- config.h
+++ config.h
@@ -9,15 +9,15 @@ static const int topbar             = 1;        /* 0 means bottom bar */
 static const char *fonts[]          = { "Fira Code:size=12:style=bold", "Fontawesome:size=12" };
 static const char dmenufont[]       = "Fira Code:size=12";
 // background colour
-static const char col_gray1[]       = "#222222";
+static const char col_gray1[]       = "#282828";
 // inactive window border colour
-static const char col_gray2[]       = "#444444";
+static const char col_gray2[]       = "#928374";
 // font colour
-static const char col_gray3[]       = "#bbbbbb";
+static const char col_gray3[]       = "#a89984";
 // current tag and current window font colour 
-static const char col_gray4[]       = "#eeeeee";
+static const char col_gray4[]       = "#ebdbb2";
 // top bar second colour and active window border colour
-static const char col_cyan[]        = "#005577";
+statie const char col_cyan[]        = "#d79921";
 static const char *colors[][3]      = {
 	/*               fg         bg         border   */
 	[SchemeNorm] = { col_gray3, col_gray1, col_gray2 },
